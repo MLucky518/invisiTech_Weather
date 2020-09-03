7d059c2dd68a986b8aa8a393acc2dcd1d01aeffd
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2020-09-03 18:16:06 -0400

    form completed, fetching results and logging correctly

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,28 +1,46 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import Form from './Form';
 
 function App() {
-  const [input,setInput] = useState({cityName:"",city_zip:""})
+  
+  
   
 
-  const getWeather = (zip) =>{
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${process.env.REACT_APP_API_KEY}`)
+  const getWeatherByZip = (input) =>{
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${input},us&appid=${process.env.REACT_APP_API_KEY}`)
     .then(res =>{
-      console.log(res.data)
+      console.log("RESULTS BY ZIP:")
+      console.log("CITY_NAME:",res.data.name)
+      console.log("CURRENT WEATHER:",res.data.weather[0].main)
+      console.log("DESCRIPTION:",res.data.weather[0].description)
+      console.log("CURRENT DT: (datetime)",res.data.dt)
     })
     .catch(err =>{
-      console.log(err)
+      console.log("Error loading by Zip",err)
+    })
+  }
+  const getWeatherByName = (input) =>{
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${input},us&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(res =>{
+      console.log("RESULTS BY NAME:")
+      console.log("CITY_NAME:",res.data.name)
+      console.log("CURRENT WEATHER:",res.data.weather[0].main)
+      console.log("DESCRIPTION:",res.data.weather[0].description)
+      console.log("CURRENT DT: (datetime)",res.data.dt)
+    })
+    .catch(err =>{
+      console.log("Error loading by Name",err)
     })
   }
 
-  useEffect(() => {
-    getWeather(12189)
-  }, [])
-  
+
   return (
     <div className="App">
       <h1>Weather</h1>
+      <Form getWeatherByZip = {getWeatherByZip} getWeatherByName={getWeatherByName}/>
+      
     </div>
   );
 }
--- weather/src/Form.jsx
+++ weather/src/Form.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Form() {
-    return (
-        <div>
-            <h1>Form</h1>
-        </div>
-    )
+function Form({ getWeatherByZip, getWeatherByName }) {
+  const [input, setInput] = useState({ cityName: "", cityZip: "" });
+
+  const handleChanges = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitName = (e) => {
+    e.preventDefault();
+    getWeatherByName(input.cityName);
+  };
+
+  const handleSubmitZip = (e) => {
+    e.preventDefault();
+    getWeatherByZip(input.cityZip);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmitName}>
+        <input
+          name="cityName"
+          type="text"
+          placeholder="Enter City Name"
+          value={input.cityName}
+          onChange={handleChanges}
+        ></input>
+          <input type="submit" />
+      </form>
+      <form onSubmit={handleSubmitZip}>
+        <input
+          name="cityZip"
+          type="text"
+          placeholder="Enter City Zipcode"
+          value={input.cityZip}
+          onChange={handleChanges}
+        ></input>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
