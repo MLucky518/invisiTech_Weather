e4a7e1953c1e535c80b8559735ab3347b3e8f482
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2020-09-03 17:38:26 -0400

    API calls working/env folder

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,7 +1,24 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [input,setInput] = useState({cityName:"",city_zip:""})
+  
+
+  const getWeather = (zip) =>{
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(res =>{
+      console.log(res.data)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    getWeather(12189)
+  }, [])
   
   return (
     <div className="App">
